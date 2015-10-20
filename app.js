a5d18a4ada5e0facfcfa0de5b65925b6db1ab8e2
Benj Sicam <bsicam@reekoh.com>
2015-10-20 20:50:33 +0800

    Added logging and error handling + validations

--- app.js
+++ app.js
@@ -1,42 +1,56 @@
 'use strict';
 
-var _        = require('lodash'),
-	path     = require('path'),
-	uuid     = require('node-uuid'),
-	knox     = require('knox'),
+var path     = require('path'),
 	platform = require('./platform'),
-	config   = require('./config.json'),
 	s3Client, s3Path;
 
 /*
  * Listen for the data event.
  */
 platform.on('data', function (data) {
-	var fileName = data.s3FileName || uuid.v4() + '.json';
-	var filePath = path.join(s3Path, fileName);
+	var isJSON = require('is-json');
 
-	if (data.s3FolderPath)
-		filePath = path.join(path.resolve(data.s3FolderPath), fileName);
+	if (isJSON(data, true)) {
+		var uuid = require('node-uuid');
+		var fileName = data.s3FileName || uuid.v4() + '.json';
+		var filePath = path.join(s3Path, fileName);
 
-	delete data.s3FileName;
-	delete data.s3FolderPath;
+		if (data.s3FolderPath)
+			filePath = path.join(path.resolve(data.s3FolderPath), fileName);
 
-	s3Client.putBuffer(new Buffer(JSON.stringify(data, null, 4)), filePath, {
-		'Content-Type': 'application/json'
-	}, function (error, response) {
-		if (error)
-			platform.handleException(error);
-		else if (response.statusCode !== 200) {
-			console.error('Error on AWS S3.', response.statusMessage);
-			platform.handleException(new Error(response.statusMessage));
-		}
-	});
+		delete data.s3FileName;
+		delete data.s3FolderPath;
+
+		s3Client.putBuffer(new Buffer(JSON.stringify(data, null, 4)), filePath, {
+			'Content-Type': 'application/json'
+		}, function (error, response) {
+			if (error)
+				platform.handleException(error);
+			else if (response.statusCode !== 200) {
+				console.error('Error on AWS S3.', response.statusMessage);
+				platform.handleException(new Error(response.statusMessage));
+			}
+			else {
+				platform.log(JSON.stringify({
+					title: 'Added JSON file to AWS S3',
+					file: filePath,
+					data: data
+				}));
+			}
+		});
+	}
+	else
+		platform.handleException(new Error('Invalid data received. ' + data));
 });
 
 /*
  * Listen for the ready event.
  */
 platform.once('ready', function (options) {
+	var _      = require('lodash'),
+		knox   = require('knox'),
+		config = require('./config.json');
+
 	s3Path = _.isEmpty(options.path) ? config.path.default : path.resolve('/' + options.path);
 
 	s3Client = knox.createClient({
@@ -46,5 +60,6 @@ platform.once('ready', function (options) {
 		region: options.region || config.region.default
 	});
 
+	platform.log('S3 Storage Initialized.');
 	platform.notifyReady();
 });
